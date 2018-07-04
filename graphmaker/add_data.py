b1a8357cf58d3c50bdb6b8d52df1bc5ae2351e0f
Max Hully <max.hully@gmail.com>
2018-07-04 11:01:00 -0400

    Adding more data

--- graphmaker/add_data.py
+++ graphmaker/add_data.py
@@ -1,28 +1,32 @@
+import os
+
+import geopandas as gp
+
 from add_columns import add_columns_and_report
 from main import load_graph
 
-import os
-import geopandas as gp
 
 def zfill2(n):
     return str(n).zfill(2)
 
-states = os.listdir('./graphs/')
+
+states = [folder for folder in os.listdir('./graphs/') if folder.isdigit()]
+
 
 def get_graph_paths(fips):
     return ['./graphs/' + fips + '/rook.json',
             './graphs/' + fips + '/queen.json']
 
+
 def get_shape_path(fips):
     return '/'.join(['.', 'tiger_data', fips,
                      'tl_2012_' + fips + '_vtd10.shp'])
 
-graph = None
-data = None
+
 for state in states:
     data = gp.read_file(get_shape_path(state))
     for path in get_graph_paths(state):
         graph = load_graph(path)
-        result = add_columns_and_report(graph, data, ['ALAND10', 'AWATER10', 'NAME10'], 'GEOID10')
+        result = add_columns_and_report(
+            graph, data, ['ALAND10', 'AWATER10', 'NAME10'], 'GEOID10')
         print(result)
-
