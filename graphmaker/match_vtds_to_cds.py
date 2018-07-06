f747d879f159603e5fc227429e32dd4361537c37
Max Hully <max.hully@gmail.com>
2018-07-06 13:04:08 -0400

    Matching works!

--- graphmaker/match_vtds_to_cds.py
+++ graphmaker/match_vtds_to_cds.py
@@ -1,7 +1,14 @@
-import collections
-import csv
+import logging
+from collections import Counter
+
+import numpy
+import pandas
 
 from constants import fips_to_state_abbreviation
+from main import load_graph
+
+logging.basicConfig(level=logging.INFO)
+
 
 # The VTDs have GEOIDs of this form:
 # {2-digit state FIPS}{3-digit county code}{>=2-digit VTD code}
@@ -27,54 +34,75 @@ def block_to_unit_filepath(fips, unit):
 # Our objective will be to make a VTD-to-CD assignment CSV.
 
 
-def match_blocks_to_cds(fips):
-    matching = dict()
-    with open(block_to_unit_filepath(fips, 'CD')) as f:
-        next(f)
-        for line in f:
-            block, cd = line.strip().split(',')
-            if not cd:
-                print("cd was blank for " + block)
-            else:
-                matching[block] = cd
-    return matching
+def blocks_to_vtds_dataframe(fips, name_geoid_column='geoid'):
+    df = pandas.read_csv(block_to_unit_filepath(
+        fips, 'VTD'), index_col='BLOCKID', dtype=str)
+    df[name_geoid_column] = fips + df['COUNTYFP'] + df['DISTRICT']
+    return df
 
 
-def match_vtds_to_blocks(fips):
-    matching = collections.defaultdict(list)
-    with open(block_to_unit_filepath(fips, 'VTD')) as f:
-        # ignore the column headers
-        next(f)
-        for line in f:
-            block, county, vtd = line.strip().split(',')
-            geoid = ''.join([fips, county, vtd])
-            matching[geoid].append(block)
-    return matching
+def integrate_over_blocks_in_vtds(fips, series, function=numpy.sum):
+    blocks = blocks_to_vtds_dataframe(fips)
+    blocks['data'] = series
+    grouped_by_vtd = blocks.groupby('geoid')
+    vtd_totals = grouped_by_vtd['data'].aggregate(function)
+    return vtd_totals
+
+
+def patch_value_from_neighbors(node, series, graph):
+    neighbor_values = Counter(series[neighbor]
+                              for neighbor in graph.neighbors(node))
+
+    best_guess, count = neighbor_values.most_common(1)[0]
+    degree = graph.degree[node]
+    percent = round(count/degree, 2) * 100
+
+    logging.info(f"{percent}% of {node}'s neighbors have value {best_guess},"
+                 f" so we our guessed value for {node} is {best_guess}.")
+
+    return best_guess
+
+
+def choose_most_common(group):
+    most_common = group.value_counts(dropna=False).index[0]
+    return most_common
+
+
+def graph_path(fips, adjacency='rook'):
+    base = "./graphs/vtd-adjacency-graphs/vtd-adjacency-graphs/"
+    return base + fips + "/" + adjacency + ".json"
 
 
 def match_vtds_to_cds(fips):
-    vtds_to_blocks = match_vtds_to_blocks(fips)
-    blocks_to_cds = match_blocks_to_cds(fips)
+    logging.info("Loading blocks for fips code " + fips)
+    blocks_to_cds = pandas.read_csv(
+        block_to_unit_filepath(fips, 'CD'), index_col='BLOCKID', dtype=str)
 
-    vtds_to_cds = dict()
-    for vtd in vtds_to_blocks:
-        for block in vtds_to_blocks[vtd]:
-            if block in blocks_to_cds:
-                vtds_to_cds[vtd] = blocks_to_cds[block]
+    blocks_to_vtds = blocks_to_vtds_dataframe(fips)
+    blocks_to_vtds['cd'] = blocks_to_cds['DISTRICT']
+
+    logging.info(
+        "Matching each VTD to the most common CD assignment of the blocks in the VTD.")
+
+    grouped_by_vtd = blocks_to_vtds.groupby('geoid')
+    vtds_to_cds = grouped_by_vtd['cd'].aggregate(choose_most_common)
+
+    graph = load_graph(graph_path(fips))
+    for node in vtds_to_cds[vtds_to_cds.isna()].index:
+        vtds_to_cds[node] = patch_value_from_neighbors(
+            node, vtds_to_cds, graph)
 
     return vtds_to_cds
 
 
-def write_matching_to_csv(matching, units, filepath):
-    with open(filepath, 'w', newline='') as f:
-        writer = csv.writer(f, quoting=csv.QUOTE_NONE)
-        writer.writerow(units)
-        writer.writerows(sorted(matching.items()))
+def create_matching_for_state(fips, output_file):
+    vtds_to_cds = match_vtds_to_cds(fips)
 
+    logging.info("I created a matching of VTDs to CDs.")
+    logging.info(vtds_to_cds[vtds_to_cds.isna()])
 
-def create_matching_for_state(fips, output_filepath):
-    matching = match_vtds_to_cds(fips)
-    write_matching_to_csv(matching, ['VTD', 'CD'], output_filepath)
+    logging.info("Writing output to " + output_file)
+    vtds_to_cds.to_csv(output_file)
 
 
 def main():
