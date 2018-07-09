1d68ca1c63923eb28f0ab56703841ccdfd29f9aa
Max Hully <max.hully@gmail.com>
2018-07-08 20:10:48 -0400

    Made a chloropleth module in reports

---
+++ graphmaker/reports/chloropleth.py
@@ -0,0 +1,25 @@
+import matplotlib
+from graphmaker.resources import VTDShapefile
+from graphmaker.geospatial import reprojected
+
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt  # noqa: E402
+
+
+def chloropleth(fips, column, filepath='./output.png'):
+    df = VTDShapefile(fips).as_df()
+    df = reprojected(df)
+
+    if isinstance(column, str):
+        column_name = column
+    else:
+        column_name = 'column'
+        df['column'] = column
+
+    _, ax = matplotlib.subplots(1)
+    df.plot(ax=ax, linewidth=0.5, edgecolor='0.5', column=column_name)
+    ax.set_axis_off()
+
+    plt.axis('equal')
+    plt.savefig(filepath)
